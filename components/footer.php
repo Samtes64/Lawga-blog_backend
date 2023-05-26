f830716605b96c0c2988e7fb24641d94f541d280
Samuel Tesfaye <samtes64@gmail.com>
2023-05-26 22:54:11 +0300

    editing the php pages

---
+++ components/footer.php
@@ -0,0 +1,36 @@
+
+
+<footer>
+      <div class="footer_socials">
+        <a href="https://github.com/Samtes64"><i class="uil uil-github"></i></a>
+        <a href="https://t.me/Samuel_tes"><i class="uil uil-telegram"></i></a>
+        <a href="https://www.linkedin.com/in/samuel-tesfaye-174aa724b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bi5PX%2Fw2ORZKoYPLwTH5XJA%3D%3D"><i class="uil uil-linkedin"></i></a>
+      </div>
+
+      <div class="container footer_container">
+
+        <article>
+          
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </article>
+
+      </div>
+      <div class="footer_copyright">
+        <small>copyright &copy; Lawga blog </small>
+      </div>
+
+    </footer>
+
+    <script src="./main.js"></script>
+
+
+
+    
+  </body>
+</html>
