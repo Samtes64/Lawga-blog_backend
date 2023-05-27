3b69cb86b3f64703c484ba37870f68b6ec3e8a48
Samuel Tesfaye <samtes64@gmail.com>
2023-05-27 08:43:45 +0300

    put the header and footer as a constant to use in all other pages

--- category-posts.php
+++ category-posts.php
@@ -1,47 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Lawga Blog</title>
-    <link rel="stylesheet" href="./style.css" />
-    <link
-      rel="stylesheet"
-      href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-  <body>
-    <nav>
-      <div class="cantainer nav_container">
-        <a href="index.html" class="nav_logo"
-          >La<strong style="color: var(--color-1); font-size: large"
-            >W</strong
-          >ga</a
-        >
-        <ul class="nav_items">
-          <li><a href="blog.html">Blog</a></li>
-          <!-- <li><a href="signin.html">Signin</a></li> -->
-          <li class="nav_profile">
-            <div class="avatar">
-              <img src="./images/avatar.JPG" alt="avatar" />
-            </div>
-            <ul>
-              <li><a href="posts.html">Posts</a></li>
-              <li style="background: var(--color-dark1)">
-                <a href="logout.html">Logout</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-        <button id="open_nav"><i class="uil uil-bars"></i></button>
-        <button id="close_nav"><i class="uil uil-multiply"></i></button>
-      </div>
-    </nav>
+<?php
+include 'components/header.php'
+?>
 
 
 <header class="category_title">
@@ -173,37 +132,6 @@
       </div>
     </section>
 
-    <footer>
-      <div class="footer_socials">
-        <a href="https://github.com/Samtes64"><i class="uil uil-github"></i></a>
-        <a href="https://t.me/Samuel_tes"><i class="uil uil-telegram"></i></a>
-        <a href="https://www.linkedin.com/in/samuel-tesfaye-174aa724b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bi5PX%2Fw2ORZKoYPLwTH5XJA%3D%3D"><i class="uil uil-linkedin"></i></a>
-      </div>
-
-      <div class="container footer_container">
-
-        <article>
-          
-          <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Blog</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">Contact</a></li>
-          </ul>
-        </article>
-
-      </div>
-      <div class="footer_copyright">
-        <small>copyright &copy; Lawga blog </small>
-      </div>
-
-    </footer>
-
-    <script src="./main.js"></script>
-
-
-
-    
-  </body>
-</html>
+<?php
+include 'components/footer.php'
+?>
--- components/footer.php
+++ components/footer.php
@@ -27,7 +27,7 @@
 
     </footer>
 
-    <script src="./main.js"></script>
+    <script src="./js/main.js"></script>
 
 
 
