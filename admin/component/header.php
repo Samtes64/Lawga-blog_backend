22a8e314d998579b0f3768573304c07453e79a86
Samuel Tesfaye <samtes64@gmail.com>
2023-05-28 17:29:14 +0300

    restructuring the admin pages

---
+++ admin/component/header.php
@@ -0,0 +1,48 @@
+<?php
+require 'config/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Lawga Blog</title>
+    <link rel="stylesheet" href="<?= ROOT_URL ?>/css/style.css" />
+    <link
+      rel="stylesheet"
+      href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <nav>
+      <div class="cantainer nav_container">
+        <a href="<?= ROOT_URL ?>" class="nav_logo"
+          >La<strong style="color: var(--color-1); font-size: large"
+            >W</strong
+          >ga</a
+        >
+        <ul class="nav_items">
+          <li><a href="blog.php">Blog</a></li>
+          <!-- <li><a href="signin.php">Signin</a></li> -->
+          <li class="nav_profile">
+            <div class="avatar">
+              <img src="./images/avatar.JPG" alt="avatar" />
+            </div>
+            <ul>
+              <li><a href="dashboard.php">posts</a></li>
+              <li style="background: var(--color-dark1)">
+                <a href="signin.php">Logout</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <button id="open_nav"><i class="uil uil-bars"></i></button>
+        <button id="close_nav"><i class="uil uil-multiply"></i></button>
+      </div>
+    </nav>
